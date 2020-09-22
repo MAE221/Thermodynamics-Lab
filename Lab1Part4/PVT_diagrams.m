b20beb7310ae9af37ea0a7ce6e893d24dafb5223
Daniel Dudt <33005725+ddudt@users.noreply.github.com>
2020-09-22 10:58:08 -0400

    Update PVT_diagrams.m

--- Lab1Part4/PVT_diagrams.m
+++ Lab1Part4/PVT_diagrams.m
@@ -1,13 +1,11 @@
-clc, clearvars
-
 % Enter your Photon name%
-name = 'MAE221-Photon-Dudt';
+name = 'PHOTON_NAME';
 
 % Enter the unique access token for your Photon%
-atoken = 'c429c624606fbae0992aebef1aad0eb2f1fc1d4b';
+atoken = 'ACCESS_TOKEN';
 
 % Enter the serial port for your Photon. If you have trouble finding the port or connecting to the serial, leave this blank.
-port = 'COM3';
+port = 'PORT';
 
 % Enter the input ports for your device.
 presPin = 'A0';
@@ -16,16 +14,13 @@ tempPin = 'A1';
 % Enter true if you are using a serial connection and false if you are using a cloud connection. If you having issues, just put false.
 isUsingSerial = true;
 
-volm = (150:-40:70)'; % mL
-samples = 5;
+%%
 
 g = Photon(name, atoken, port);
 
 if isUsingSerial
     g.disconnect;
 end
-
-%%
 
 % helpful var set
 for i = 0:7
@@ -37,60 +32,83 @@ for i = 0:7
     eval(msg);
 end
 
-%%
+%% Collect data from the Photon
 
-presVolt = zeros(size(volm));
-tempVolt = zeros(size(volm));
+% Enter the Volumes you are measuring at as an array
+volumes = (150:-40:70)'; % mL
+samples = 5;  % number of voltage samples to average over at each volume
 
-for i = 1:length(volm)
-    fprintf('Move plunger to %3d mL\n',volm(i))
-    pause(1)
+presVolt = zeros(size(volumes));  % array to store Pressure voltages
+tempVolt = zeros(size(volumes));  % array to store Temperature voltages
+
+% loop through each Volume measurement
+for i = 1:length(volumes)
+    fprintf('Move plunger to %3d mL\n',volumes(i))
+    pause(2)
     
-    voltagesIn = zeros(5,2);
+    % read Voltage samples from Photon pins
+    voltagesIn = zeros(samples,2);
     for j = 1:samples
         voltagesIn(j,1) = g.analogRead(presPin);
         voltagesIn(j,2) = g.analogRead(tempPin);
     end
+    % average the Voltage readings over the number of samples
     presVolt(i) = mean(voltagesIn(:,1));
     tempVolt(i) = mean(voltagesIn(:,2));
     
 end
 
-[pres,temp,PVT] = calcPVT(volm,presVolt,tempVolt);
-
-% uncertainty in P*V/T (psi*mL/K)
-dvolm = 1;
-dpres = 0.1*pres;  % 10% error 
-dtemp = 1;  % +/- 1 C
-err = PVT.*sqrt((dpres./pres).^2 + (dvolm./volm).^2 + (dtemp./temp).^2);
-
 clear g
 
-%%
+%% Convert the Voltage data to Pressure and Temperature measurements
 
+pres = calcPres(presVolt);  % calculate Pressure (psi)
+temp = calcTemp(tempVolt);  % calculate Temperature (K)
+PVT = pres.*volumes./temp;  % calculate P*V/T (psi*mL/K)
+
+%% Calculate the uncertainty
+
+% uncertainty in each measurement
+d_volm = 1;         % +/- 1 mL
+d_pres = 0.1*pres;  % 10% error 
+d_temp = 1;         % +/- 1 C
+err = PVT.*sqrt((d_pres./pres).^2+(d_volm./volumes).^2+(d_temp./temp).^2);
+
+%% Plot your measurements
+
+% Figure showing Pressure vs Volume
+% Figure showing Temperature vs Volume
 lc = [0 0 1];  rc = [1 0 0];
 figure
 set(gcf,'color','w','defaultAxesColorOrder',[lc; rc])
 hold on, xlabel('Volume (mL)')
 yyaxis left
-plot(volm,pres,'bo','MarkerFaceColor','b','MarkerSize',6)
+plot(volumes,pres,'bo','MarkerFaceColor','b','MarkerSize',6)
 ylabel('Pressure (psi)')
 set(gca,'FontSize',16,'LineWidth',2)
 yyaxis right
-plot(volm,temp-273.15,'ro','MarkerFaceColor','r','MarkerSize',6)
+plot(volumes,temp-273.15,'ro','MarkerFaceColor','r','MarkerSize',6)
 ylabel('Temperature (C)')
 set(gca,'FontSize',16,'LineWidth',2)
 
+% Figure showing PV/T vs Volume
 figure
 set(gcf,'color','w')
 hold on, xlabel('Volume (mL)')
-errorbar(volm,PVT,err,err,'ko','MarkerFaceColor','k','MarkerSize',6,'LineWidth',2)
+errorbar(volumes,PVT,err,err,'ko','MarkerFaceColor','k','MarkerSize',6,'LineWidth',2)
 ylabel('PV/T (psi * mL / K)')
 set(gca,'FontSize',16,'LineWidth',2)
 
-%%
+%% Functions for converting Photon Voltage to Pressure & Temperature
+% You may need to edit some of the values in these functions!
 
-function [pres,temp,PVT] = calcPVT(volm,presVolt,tempVolt)
+%{
+Inputs:
+    presVolt = pressure sensor voltage from Photon (V)
+Outputs:
+    pres = pressure measurement (psi)
+%}
+function pres = calcPres(presVolt)
 
 % parameters for the Photon
 supplyVolt = 4.8;  % Photon supply voltage (V)
@@ -98,6 +116,22 @@ supplyVolt = 4.8;  % Photon supply voltage (V)
 % parameters for the pressure sensor
 presRange = 15;  % range of pressure sensor (psi)
 presRef = 14.7;  % reference/atmospheric pressure (psi)
+
+% pressure (psi)
+pres = (presVolt - 0.1*supplyVolt) .* presRange./(0.8*supplyVolt) + presRef;
+
+end
+
+%{
+Inputs:
+    tempVolt = thermistor voltage from Photon (V)
+Outputs:
+    temp = temperature measurement (K)
+%}
+function temp = calcTemp(tempVolt)
+
+% parameters for the Photon
+supplyVolt = 4.8;  % Photon supply voltage (V)
 
 % parameters for the operational amplifier
 tempRefVolt = supplyVolt/2;  % op-amp reference voltage (V)
@@ -112,22 +146,7 @@ D =-7.25e-8;
 thermVolt = tempVolt./(1 + 1e5/gainResist);
 thermResRatio = supplyVolt./(thermVolt + tempRefVolt) - 1;
 
-% pressure (psi)
-pres = (presVolt - 0.1*supplyVolt) .* presRange./(0.8*supplyVolt) + presRef;
 % temperature (K)
 temp = 1./(A + B*log(thermResRatio) + C*log(thermResRatio).^2 + D*log(thermResRatio).^3);
 
-% P*V/T (psi*mL/K)
-PVT = pres.*volm./temp;
-
 end
-
-%% Documentation
-
-%{
-    ASDX RR X 015PD AA5 pressure sensor
-    http://www.farnell.com/datasheets/1765461.pdf
-    
-    RL0503-5820-97-MS
-    https://www.digikey.com/product-detail/en/amphenol-advanced-sensors/RL0503-5820-97-MS/KC003T-ND/136365
-%}
